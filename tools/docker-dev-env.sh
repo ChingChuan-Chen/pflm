03f053a2bdba1b5dea97972cee6c1fc781bb9296
Ching-Chuan Chen <zw12356@gmail.com>
2026-03-27 23:31:14 +0800

    fix build

--- tools/docker-dev-env.sh
+++ tools/docker-dev-env.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-build_dir="build/docker-cp$(python -c 'import sys; print(str(sys.version_info.major) + str(sys.version_info.minor))')"
+python -m pip install .[tests]
+python -m pip uninstall -y pflm
 
+build_dir="build/docker-cp$(python -c 'import sys; print(str(sys.version_info.major) + str(sys.version_info.minor))')"
 python -m pip install --verbose --no-build-isolation --editable . \
     --config-settings editable-verbose=true \
     --config-settings "build-dir=${build_dir}"
-
-python -m pip install "pytest>=7.1.2" "pytest-cov>=2.9.0"
 
 if [ "$#" -eq 0 ]; then
     exec bash
